572dce5dce2664443fd2ecfe840a5b6c41f96c03
devesh1808 <devesh1808@gmail.com>
2023-10-05 13:23:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+const AppLayout = () => {
+  return(
+     {
+      /*
+      Header
+         -Logo
+         -Nav Items(Right Side)
+         -Cart
+      Body
+         -Search Bar
+         -Restraunt List
+           -Restraunt Card
+              -Image
+              -Name
+              -Rating
+              -Cuesienes
+      Footer
+          -Links
+          -CopyRight
+      */
+     }
+  )
+} 
