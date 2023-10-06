cf35f7f646ec78fdd19746d604603d8ac004f15d
devesh1808 <devesh1808@gmail.com>
2023-10-07 03:33:33 +0530

    added filter button to filter the cards and filter method and state hook study

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,31 @@
 import RestrauntCard from "./RestrauntCard";
 import restaurantList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-      <div className="restraunt-list">
-        {
-          restaurantList.map((restraunts) => (<RestrauntCard key={restraunts.data.id} restraunt={restraunts}/>))
-        }
+
+  const[resList, setResList] = useState(restaurantList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredData = restaurantList.filter((res) => res.data.avgRating > 4);
+            setResList(filteredData);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
-    );
+      <div className="restraunt-list">
+        {resList.map((restraunts) => (
+          <RestrauntCard key={restraunts.data.id} restraunt={restraunts} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
