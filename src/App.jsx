7c29ff70c31da158e8ff4dfa58cbe7bbcf54466b
devesh1808 <devesh1808@gmail.com>
2023-10-07 00:20:33 +0530

    removed extra field form restraunt card

--- src/App.jsx
+++ src/App.jsx
@@ -787,7 +787,6 @@ const RestrauntCard = (props) => {
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{costForTwo / 100} FOR TWO</h4>
-      <h4>{lastMileTravelString} minutes</h4>
       <h4>{deliveryTime} minutes</h4>
     </div>
   );
