8d92c0d62702b116dec7786212e5374dfaa25ec2
devesh1808 <devesh1808@gmail.com>
2023-10-10 13:41:00 +0530

    added login button in header using useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,37 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Title = () => (
-    <a href="/">
-      <img
-        className="logo"
-        src={LOGO_URL}
-        alt="Food Villa Logo"
-      />
-    </a>
-  );
+  <a href="/">
+    <img className="logo" src={LOGO_URL} alt="Food Villa Logo" />
+  </a>
+);
 
 const Header = () => {
-    return (
-      <div className="header">
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Header;
-  
+export default Header;
